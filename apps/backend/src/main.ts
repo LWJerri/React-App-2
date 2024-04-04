54a2c0ea4c43d6c6323093741fa55cde9acccbb0
LWJerri <loginmanx@gmail.com>
2024-04-04 16:53:22 +0200

    feat(backend): Add main file

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,33 @@
+import { RedocModule } from "@brakebein/nestjs-redoc";
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { Logger } from "nestjs-pino";
+import { AppModule } from "./modules/app/app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useLogger(app.get(Logger));
+  app.enableCors();
+
+  const logger = app.get(Logger);
+
+  const configService = app.get(ConfigService);
+  const appPort = configService.get<number | string>("PORT")!;
+
+  const options = new DocumentBuilder()
+    .setTitle("Task Manager V2 API")
+    .setDescription("Documentation with all possible API endpoints for this project.")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  await Promise.all([app.listen(appPort, "0.0.0.0"), RedocModule.setup("/", app, document, {})]);
+
+  logger.log(`🚀 API ready on ${appPort} port.`);
+}
+
+bootstrap();
