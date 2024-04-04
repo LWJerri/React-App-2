2a1bc3ad8a22909ec5c52376f9e397685d6ff6cf
LWJerri <loginmanx@gmail.com>
2024-04-04 20:58:14 +0200

    feat(backend): Remove `useLogger` & `bufferLogs` params

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,10 +7,9 @@ import { Logger } from "nestjs-pino";
 import { AppModule } from "./modules/app/app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.useLogger(app.get(Logger));
   app.enableCors();
 
   const logger = app.get(Logger);
